264053746cdbc68c31896ebd04a1b0ce7e795d4c
ShiverZm <1113673178@qq.com>
2020-11-29 23:50:12 +0800

    commit firstly

---
+++ 02HelloWinRTApp/MainPage.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::_02HelloWinRTApp::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    //void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    //{
+    //    myButton().Content(box_value(L"HelloWorld Clicked"));
+    //}
+
+    void MainPage::inputButton_Click(
+        winrt::Windows::Foundation::IInspectable const& sender,
+        winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
+    {
+        greetingOutput().Text(L"Hello, " + nameInput().Text() + L"!");
+    }
+}
